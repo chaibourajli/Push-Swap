885bf07235082282f462809b6ca3e9fafacef913
Chaimaa Bourajli <cbourajl@e3r2p10.1337.ma>
2022-06-29 23:50:39 +0100

    .

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cbourajl <cbourajl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 21:45:25 by cbourajl          #+#    #+#             */
-/*   Updated: 2022/06/29 23:35:47 by cbourajl         ###   ########.fr       */
+/*   Updated: 2022/06/29 23:50:06 by cbourajl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int main(int ac, char **av)
         return (0);
     a = create_stack(ac - 1);
     check_args(ac, av);
-    while (i < ac)
+    while (ac > i)
     {
-        push(a, ft_atoi(av[i]));
-        i++;
-        printf("%d\n", a->tab[a->top]);
+        push(a, ft_atoi(av[ac -1]));
+        ac--;
     }
+    printf("%d\n", a->tab[a->top]);
 }
