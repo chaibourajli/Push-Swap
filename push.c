62338832fe73872dbe145d6cb9dd825f2ba2b0e0
Chaimaa Bourajli <cbourajl@e1r2p15.1337.ma>
2022-07-06 18:28:31 +0100

    opdone

--- push.c
+++ push.c
@@ -6,9 +6,30 @@
 /*   By: cbourajl <cbourajl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 17:20:34 by cbourajl          #+#    #+#             */
-/*   Updated: 2022/07/06 17:09:35 by cbourajl         ###   ########.fr       */
+/*   Updated: 2022/07/06 18:00:21 by cbourajl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void    push(t_stack **a, t_stack **b, int n)
+{
+    t_stack *tmp;
+
+    if (!*b)
+    {
+        (*b) = newstack((*a)->value);
+        (*a) = (*a)->next;
+    }
+    else
+    {
+        tmp = (*a);
+        (*a) = (*a)->next;
+        tmp->next = NULL;
+        addfront(b, tmp);
+    }
+    if (n)
+        ft_putstr("pb\n");
+    else
+        ft_putstr("pa\n");
+}
--- reverse_rotate.c
+++ reverse_rotate.c
@@ -6,9 +6,61 @@
 /*   By: cbourajl <cbourajl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 17:23:11 by cbourajl          #+#    #+#             */
-/*   Updated: 2022/07/06 17:09:40 by cbourajl         ###   ########.fr       */
+/*   Updated: 2022/07/06 18:20:38 by cbourajl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void    rra(t_stack **a)
+{
+    t_stack *tmp;
+    t_stack *prev;
+
+    tmp = *(a);
+    prev = *(a);
+    while (tmp->next)
+    {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    prev->next = NULL;
+    addfront(a, tmp);
+    ft_putstr("rra\n");
+}
+
+void    rrb(t_stack **b)
+{
+    t_stack *tmp;
+    t_stack *prev;
+
+    tmp = *(b);
+    prev = *(b);
+    while (tmp->next)
+    {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    prev->next = NULL;
+    addfront(b, tmp);
+    ft_putstr("rrb\n");
+}
+
+void    rrr(t_stack **a, t_stack **b)
+{
+    if (!(*a) && !(*a)->next)
+    {
+        rrb(b);
+        ft_putstr("rrb\n");
+        exit(1);
+    }
+    if (!(*b) && !(*b)->next)
+    {
+        rra(a);
+        ft_putstr("rra\n");
+        exit(1);
+    }
+    rra(a);
+    rrb(b);
+    ft_putstr("rrr\n");
+}
