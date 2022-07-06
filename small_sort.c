ba36972658e9370b5555d8ed45f5dbc7dda125fc
Chaimaa Bourajli <cbourajl@student.1337.ma>
2022-07-06 22:58:02 +0100

    ilh

--- small_sort.c
+++ small_sort.c
@@ -12,4 +12,38 @@
 
 #include "push_swap.h"
 
-void    
+void    five_verify(t_stack **a)
+{
+    if ((*a)->index == 2)
+    {
+        if ((*a)->next->index == 1)
+        {
+            sa(*a);
+            rra(*a);
+        }
+        else if ((*a)->next->index == 0)
+            ra(a);
+    }
+}
+
+void    three(t_stack *a)
+{
+    get_index(*a);
+    if (to_order(*a))
+    {
+        if ((*a)->index == 0)
+        {
+            rra(a);
+            sa(*a);
+        }
+        else if ((*a)->index == 1)
+		{
+			if ((*a)->next->index == 2)
+				rra(a);
+			else if ((*a)->next->index == 0)
+				sa(*a);
+		}
+		else
+			five_verify(a);
+    }
+}
